832ee0f132a20f05aee7fa5c7747b9464cc11b21
Christian daheim <auriocus@gmx.de>
2021-06-30 21:25:10 +0200

    Force using "make" in CI; should fix Windows on Github Actions

--- make-ci.sh
+++ make-ci.sh
@@ -5,7 +5,9 @@ machine=$(uname -sm | tr ' ' -)
 
 # compile kbskit + sdx into build dir
 builddir="kbskit_$machine"
-MAKEFLAGS=-j4 ./kbs.tcl -r -v -builddir="$(pwd)/$builddir" install kbskit8.6 sdx
+MAKEFLAGS=-j4 ./kbs.tcl -r -v -builddir="$(pwd)/$builddir" -make=make install kbskit8.6 sdx
+# explicit -make=make is needed, because otherwise "gmake" is tried first,
+# which is alien under git-sdk on Github Actions Windows
 
 mkdir -p dist
 TARBALL=kbskit_$machine.tar.bz2
