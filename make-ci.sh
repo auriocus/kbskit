de02bd70162c86f4c93ee3772157627333260b93
Christian daheim <auriocus@gmx.de>
2021-06-30 20:05:50 +0200

    Exclude build directories (object files) from tarball

---
+++ make-ci.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# 
+
+machine=$(uname -sm | tr ' ' -)
+
+# compile kbskit + sdx into build dir
+builddir="kbskit_$machine"
+MAKEFLAGS=-j4 ./kbs.tcl -r -builddir="$builddir" install kbskit8.6 sdx
+
+mkdir -p dist
+TARBALL=kbskit_$machine.tar.bz2
+tar cvjf "dist/$TARBALL" "$builddir/bin" "$builddir/include" "$builddir/lib"
+
