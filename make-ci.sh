d64c588bc203738ff108e77eaa4e1970037ba377
Christian daheim <auriocus@gmx.de>
2022-01-03 23:41:41 +0100

    Remove rl_json (build error with autoconf on Windows)

--- make-ci.sh
+++ make-ci.sh
@@ -5,7 +5,7 @@ machine=$(uname -sm | tr ' ' -)
 
 # compile kbskit + sdx into build dir
 builddir="kbskit_$machine"
-MAKEFLAGS=-j4 ./kbs.tcl -r -v -builddir="$(pwd)/$builddir" -make=make -tar=tar install kbskit8.6 sdx tablelist6.16 img1.4.13 tkdnd2.9 photoresize0.2 rl_json0.11.1 tdom0.9.2
+MAKEFLAGS=-j4 ./kbs.tcl -r -v -builddir="$(pwd)/$builddir" -make=make -tar=tar install kbskit8.6 sdx tablelist6.16 img1.4.13 tkdnd2.9 photoresize0.2 tdom0.9.2
 # explicit -make=make is needed, because otherwise "gmake" is tried first,
 # which is alien under git-sdk on Github Actions Windows
 
