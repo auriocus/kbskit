7e05b51ad2e6f1b3ebf7814e71284e80f3fbdb7a
Christian daheim <auriocus@gmx.de>
2022-01-03 18:25:17 +0100

    Troubleshoot build issues on Windows / Github

--- kbs.tcl
+++ kbs.tcl
@@ -908,8 +908,8 @@ proc ::kbs::config::Source- {type args} {
           file delete -force $myDir.tmp
           file mkdir $myDir.tmp
           cd $myDir.tmp
-          if {$type eq {Tgz}} {Run $_(exec-gzip) -dc $args | $_(exec-tar) xf -}
-          if {$type eq {Tbz2}} {Run $_(exec-bzip2) -dc $args | $_(exec-tar) xf -}
+          if {$type eq {Tgz}} {Run $_(exec-gzip) -dc $args | $_(exec-tar) xvf -}
+          if {$type eq {Tbz2}} {Run $_(exec-bzip2) -dc $args | $_(exec-tar) xvf -}
           if {$type eq {Zip}} {Run $_(exec-unzip) $args}
           cd [file join $maindir sources]
           set myList [glob $myDir.tmp/*]
