8ec83e36606c3923ed57d14bb6438df932c80200
Christian daheim <auriocus@gmx.de>
2022-05-09 08:27:37 +0200

    Add cleanup of build files

--- make-ci.sh
+++ make-ci.sh
@@ -2,12 +2,17 @@
 # 
 
 machine=$(uname -sm | tr ' ' -)
+PKGS="sdx tablelist6.16 img1.4.13 tkdnd2.9 photoresize0.2 tdom0.9.3 vectcl0.3 vectcltk0.2 tclcurl"
+
 
 # compile kbskit + sdx into build dir
 builddir="kbskit_$machine"
-MAKEFLAGS=-j4 ./kbs.tcl -r -v -builddir="$(pwd)/$builddir" -make=make -tar=tar install kbskit8.6 sdx tablelist6.16 img1.4.13 tkdnd2.9 photoresize0.2 tdom0.9.3 vectcl0.3 vectcltk0.2 tclcurl # tcltls
+MAKEFLAGS=-j4 ./kbs.tcl -r -v -builddir="$(pwd)/$builddir" -make=make -tar=tar install kbskit8.6 $PKGS
 # explicit -make=make is needed, because otherwise "gmake" is tried first,
 # which is alien under git-sdk on Github Actions Windows
+
+# remove the build files to save space 
+./kbs.tcl -r -v distclean $PKGS
 
 # exit on error
 if [ "$?" -ne 0 ]; then 
